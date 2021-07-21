0766b663d705d11a10d35ff1ac2b43a1741db118
NelsonWang <wangnelson2@gmail.com>
2021-07-21 15:39:03 -0400

    shell practice | name and hello world sample

---
+++ 2.2/shell-tutorial/myscript.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# ECHO COMMAND
+echo hello World!
+
+# VARIABLES
+# Uppercase by convention
+# Letter, numbers, underscores
+
+NAME = "Brad"
+echo "My name is $NAME"
+echo "My name is ${NAME}"
+
+read -p "Enter your name: " NAME
