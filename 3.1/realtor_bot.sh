d46813af9cb1185a674203d3b3a7cefd22ba8e2a
NelsonWang <wangnelson2@gmail.com>
2021-07-28 20:11:32 -0400

    realtor_bot  3.1 | Adding the data

--- 3.1/realtor_bot.sh
+++ 3.1/realtor_bot.sh
@@ -2,5 +2,5 @@
 #! /bin/awk
 
 
-awk -F "," 'BEGIN{sum=0}{ if(NR>1) sum = substr($2,2, length($2)-2); print sum }
+awk -F "," 'BEGIN{sum=0}{ if(NR>1) sum += substr($2,2, length($2)-2); print sum }
 END { if (NR > 0) print sum }' realestate.csv
