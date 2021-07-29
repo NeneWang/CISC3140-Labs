b8afa48a74248a8f45b536239fca515dc0ece786
NelsonWang <wangnelson2@gmail.com>
2021-07-28 21:35:38 -0400

    realtor_bot  3.1 | counting workingd

--- 3.1/realtor_bot.sh
+++ 3.1/realtor_bot.sh
@@ -9,16 +9,23 @@ func getValue(stringIn){
 
 
 BEGIN{
-  boroBrooklyn = 3; summation_fullval_brooklyn = 0; count_target_brooklyn = 0; average_target_brooklyn = 0
+  boro_brooklyn = 3; summation_fullval_brooklyn = 0; count_target_brooklyn = 0; average_target_brooklyn = 0;
+    boro_manhattan = 1; summation_fullval_manhattan = 0; count_target_manhattan = 0; average_target_manhattan = 0;
+      boro_bronx = 2; summation_fullval_bronx = 0; count_target_bronx = 0; average_target_bronx = 0;
 
 
 
  }
 
-{ if(NR>1) boro = substr($2,2, length($2)-2);
+{ if(NR>1) boro = getValue($2);
 
 
- if(boro = 3) {print $13; } }
+ if(boro == boro_brooklyn) { summation_fullval_brooklyn += getValue($13); count_target_brooklyn++  };
 
 
-END { if (NR > 0) print sum }' realestate.csv
+
+}
+
+
+
+END { print(summation_fullval_brooklyn/count_target_brooklyn) }' realestate.csv
