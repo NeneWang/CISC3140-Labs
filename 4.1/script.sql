5f72ff15674b1d8e0f3ac76ce9c568e217a91e6d
NelsonWang <wangnelson2@gmail.com>
2021-08-05 18:07:37 -0400

    rsurname almost forgot

--- 4.1/script.sql
+++ 4.1/script.sql
@@ -3,7 +3,7 @@
 -- sqlite3 lab41.db
 -- rm lab41.db
 
-CREATE TABLE student( name CHAR(255), grade INT(10));
+CREATE TABLE student( name CHAR(255), grade INT(10), surname CHAR(255));
 
 .separator ","
 .import students_data.csv student
